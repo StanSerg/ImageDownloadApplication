4451fc61555a7d41c3023aeee976a132854b5f99
StanSerg <s.stankov@andersenlab.com>
2019-04-12 19:34:45 +0300

    recycler view with single image view items;

---
+++ app/src/main/java/com/example/imagedownloadapplication/adapters/ImgRecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package com.example.imagedownloadapplication.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.example.imagedownloadapplication.R;
+
+import java.util.List;
+
+public class ImgRecyclerViewAdapter extends RecyclerView.Adapter<ImgRecyclerViewAdapter.ViewHolder> {
+
+    private List<String> urls;
+    private Context context;
+
+    public ImgRecyclerViewAdapter(List<String> urls, Context context) {
+        this.urls = urls;
+        this.context = context;
+    }
+
+    @Override
+    public ImgRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        Glide.with(this.context)
+                .load(this.urls.get(position))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.urls.size();
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private ImageView image;
+
+        private ViewHolder(View v) {
+            super(v);
+            this.image = (ImageView) v.findViewById(R.id.iv);
+        }
+
+        private ImageView getImage() {
+            return this.image;
+        }
+    }
+
+}
--- app/src/main/java/com/example/imagedownloadapplication/ui/MainActivity.java
+++ app/src/main/java/com/example/imagedownloadapplication/ui/MainActivity.java
@@ -3,17 +3,15 @@ package com.example.imagedownloadapplication.ui;
 import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.example.imagedownloadapplication.R;
-import com.example.imagedownloadapplication.model.ChildData;
-import com.example.imagedownloadapplication.model.Data;
+import com.example.imagedownloadapplication.adapters.ImgRecyclerViewAdapter;
 import com.example.imagedownloadapplication.network.GetImgInteractorImpl;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -21,9 +19,10 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
 
     private final String TAG = MainActivity.class.getSimpleName();
     private MainContract.Presenter presenter;
-    private TextView tvInfo;
     private Button btnTop;
     private Button btnNew;
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +33,15 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     }
 
     private void initViewElements() {
-        this.tvInfo = (TextView) findViewById(R.id.tvInfo);
         this.btnNew = (Button) findViewById(R.id.btnNew);
         this.btnNew.setOnClickListener((v) -> this.presenter.onNewButtonClick());
         this.btnTop = (Button) findViewById(R.id.btnTop);
         this.btnTop.setOnClickListener((v) -> this.presenter.onTopButtonClick());
+        this.recyclerView = (RecyclerView) findViewById(R.id.rvImages);
+
+        RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
+        this.recyclerView.setLayoutManager(manager);
+
     }
 
     private void attachPresenter() {
@@ -63,14 +66,14 @@ public class MainActivity extends AppCompatActivity implements MainContract.View
     @SuppressLint("SetTextI18n")
     @Override
     public void setDataToView(List<String> listChildUrl) {
-        if (this.tvInfo == null) initViewElements();
-        this.tvInfo.setText("GET INFORMATION\n " + ((listChildUrl.size()==0) ? "": Collections.singletonList(listChildUrl)));
+        if (this.recyclerView == null) initViewElements();
+        ImgRecyclerViewAdapter adapter = new ImgRecyclerViewAdapter(listChildUrl, this);
+        this.recyclerView.setAdapter(adapter);
         Log.d(TAG, "GET INFORMATION\n " + Collections.singletonList(listChildUrl));
     }
 
     @Override
     public void onResponseFailure(Throwable throwable) {
-        this.tvInfo.setText(throwable.getMessage());
         Log.d(TAG, throwable.getMessage());
     }
 }
